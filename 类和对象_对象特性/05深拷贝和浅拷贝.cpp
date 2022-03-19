de87e949048c26d1079b7e430eb6b0dbe84c0f52
1loveman <911137865@qq.com>
2022-03-19 19:06:55 +0800

    2022.03.19

--- 类和对象_对象特性/05深拷贝和浅拷贝.cpp
+++ 类和对象_对象特性/05深拷贝和浅拷贝.cpp
@@ -11,12 +11,22 @@ public:
 		m_Weight = new int(weight);
 		cout << "有参构造函数" << endl;
 	}
-	/*person(const person& p) {
+	person(const person& p) {//浅拷贝
 		m_H = p.m_H;
 		m_Weight = p.m_Weight;
 		cout << "拷贝构造函数" << endl;
-	}*/
+	}
+	//person(const person& p) {
+	//	//深拷贝
+	//	m_H = p.m_H;
+	//	m_Weight = new int(*p.m_Weight);
+	//	cout << "深拷贝构造函数" << endl;
+	//}
 	~person() {
+		if(m_Weight!=NULL){
+			delete m_Weight;
+			m_Weight = NULL;
+		}
 		cout << "析构函数" << endl;
 	}
 public:
@@ -24,15 +34,18 @@ public:
 	int* m_Weight;
 };
 
-
-
-int main() {
-	//深拷贝与浅拷贝
-	//编译器提供的为浅拷贝，当成员属性为指针时且空间为开辟的堆，当拷贝过后析构函数释放会造成非法访问
+void test() {
 	person p(10, 20);
 	cout << *p.m_Weight << endl;
 	person p1(p);
 	cout << *p1.m_Weight << endl;
+}
+
+int main() {
+	//深拷贝与浅拷贝
+	//编译器提供的为浅拷贝，当成员属性为指针时且空间为开辟的堆，当浅拷贝过后析构函数释放会重复释放同一堆区的内存造成非法访问
+	//解决方法 拷贝时将作为深拷贝 不拷贝指针地址，而是重新开辟一段堆区内存空间将值拷入
+	test();
 	system("pause");
 	return 0;
 }
